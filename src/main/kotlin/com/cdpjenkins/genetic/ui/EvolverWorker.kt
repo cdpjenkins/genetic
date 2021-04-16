08500daa1a1568c0f300e94d830729fa5ba319d8
cdpjenkins <cdpjenkins@gmail.com>
2021-04-16 21:02:14 +0100

    Use a SpringWorker

---
+++ src/main/kotlin/com/cdpjenkins/genetic/ui/EvolverWorker.kt
@@ -0,0 +1,30 @@
+package com.cdpjenkins.genetic.ui
+
+import com.cdpjenkins.genetic.model.Evolver
+import com.cdpjenkins.genetic.model.EvolverListener
+import com.cdpjenkins.genetic.model.Individual
+import javax.swing.SwingWorker
+
+class EvolverWorker(val evolver: Evolver) : SwingWorker<Void?, Individual>() {
+    private var listeners: MutableList<EvolverListener> = mutableListOf()
+
+    fun addListener(listener: EvolverListener) {
+        listeners.add(listener)
+    }
+
+    override fun doInBackground(): Void? {
+        evolver.addListener { publish(it) }
+        while (!isCancelled) {
+            evolver.mutate()
+        }
+        return null
+    }
+
+    override fun process(chunks: MutableList<Individual>) {
+        chunks.forEach { chunk ->
+            listeners.forEach { listener ->
+                listener.invoke(chunk)
+            }
+        }
+    }
+}
--- src/main/kotlin/com/cdpjenkins/genetic/ui/GUI.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/ui/GUI.kt
@@ -1,13 +1,13 @@
 package com.cdpjenkins.genetic.ui
 
-import com.cdpjenkins.genetic.model.*
+import com.cdpjenkins.genetic.model.EvolverListener
+import com.cdpjenkins.genetic.model.makeEvolver
 import java.awt.BorderLayout
 import java.awt.LayoutManager
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.*
-import kotlin.system.measureTimeMillis
 
 class GUI(title: String? = "Genetic!") : JFrame(title) {
     constructor(): this("Genetic!") {
@@ -19,7 +19,7 @@ class GUI(title: String? = "Genetic!") : JFrame(title) {
         val masterImage: BufferedImage = ImageIO.read(File("cow.jpg"))
         val masterIcon: ImageIcon = ImageIcon(masterImage)
 
-        constructor(): this(BorderLayout()) {
+        constructor() : this(BorderLayout()) {
             val width = masterImage.width
             val height = masterImage.height
 
@@ -32,33 +32,21 @@ class GUI(title: String? = "Genetic!") : JFrame(title) {
             add(fitnessLabel, BorderLayout.SOUTH)
 
             val evolver = makeEvolver(width, height, masterImage)
-            evolver.addListener {
+            val swingWorker = EvolverWorker(evolver)
+            swingWorker.addListener {
                 individualImageLabel.icon = ImageIcon(it.bufferedImage)
                 fitnessLabel.text = "Generation: ${it.generation} Fitness: ${it.fitness}"
                 individualImageLabel.invalidate()
                 repaint()
             }
-            evolver.addListener {
+            swingWorker.addListener {
                 if (it.generation % 10 == 0) {
                     val outputFile =
                         File(String.format("output/cow_%010d.png", it.generation))
                     ImageIO.write(it.bufferedImage, "png", outputFile)
                 }
             }
-
-            startTimer(evolver)
-        }
-
-        private fun startTimer(evolver: Evolver) {
-            val timer = Timer(20) { e ->
-                val timeInMillis = measureTimeMillis {
-                    evolver.mutate()
-                    repaint()
-                }
-//                println("time taken: $timeInMillis")
-            }
-            timer.setInitialDelay(1000)
-            timer.start()
+            swingWorker.execute()
         }
     }
 }
