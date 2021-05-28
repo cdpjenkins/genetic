b548483715a3f1b93163d383897fc6e393919029
cdpjenkins <cdpjenkins@gmail.com>
2021-05-28 20:11:27 +0100

    Load initial Individual from DudeStore

--- src/main/kotlin/com/cdpjenkins/genetic/Main.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/Main.kt
@@ -2,20 +2,33 @@ package com.cdpjenkins.genetic
 
 import MASTER_IMAGE_FILE
 import com.cdpjenkins.genetic.json.deserialiseFromFile
+import com.cdpjenkins.genetic.json.deserialiseIndividual
+import com.cdpjenkins.genetic.json.serialise
+import com.cdpjenkins.genetic.model.Individual
 import com.cdpjenkins.genetic.model.makeEvolver
 import com.cdpjenkins.genetic.model.saveToDisk
 import com.cdpjenkins.genetic.ui.GUI
+import org.http4k.asString
+import org.http4k.client.OkHttp
+import org.http4k.core.Body
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.format.Jackson.auto
 import java.awt.GraphicsEnvironment
 import java.io.File
 import javax.imageio.ImageIO
 
 
 fun main(args: Array<String>) {
-
     val initialIndividual = if (args.size == 1) {
         deserialiseFromFile(File(args[0]))
     } else {
-        null
+        val response =
+            OkHttp()(Request(Method.GET, "https://genetic-dude.herokuapp.com/dude/steve?type=json"))
+        println(response)
+
+        val individualLens = Body.auto<Individual>().toLens()
+        individualLens(response)
     }
 
     val masterImage = ImageIO.read(File(MASTER_IMAGE_FILE))
@@ -27,6 +40,16 @@ fun main(args: Array<String>) {
     }
 
     evolver.addListener { it.saveToDisk() }
+
+    evolver.addListener {
+        val secret = System.getenv("SECRET")
+        val response = OkHttp()(
+            Request(Method.POST, "https://genetic-dude.herokuapp.com/dude/steve?secret=$secret")
+                .body(serialise(it))
+        )
+        println(response)
+
+    }
 
     evolver.start()
 }
