5b1b685f154a3ac1f04a0d794258f3a9cf53ef47
cdpjenkins <cdpjenkins@gmail.com>
2021-06-26 22:26:25 +0100

    Move main logic into application class

---
+++ src/main/kotlin/com/cdpjenkins/genetic/GeneticApplication.kt
@@ -0,0 +1,50 @@
+package com.cdpjenkins.genetic
+
+import EvolverSettings
+import com.cdpjenkins.genetic.dudestore.client.DudeStoreClient
+import com.cdpjenkins.genetic.model.Evolver
+import com.cdpjenkins.genetic.model.makeEvolver
+import com.cdpjenkins.genetic.persistence.S3Client
+import com.cdpjenkins.genetic.ui.GUI
+import java.awt.GraphicsEnvironment
+import java.io.File
+import javax.imageio.ImageIO
+
+class GeneticApplication(
+    val name: String,
+    val evolver: Evolver
+) {
+
+    fun start() {
+        evolver.start()
+    }
+
+    companion object {
+        fun create(name: String, secret: String): GeneticApplication {
+
+            val dudeClient = DudeStoreClient("https://genetic-dude.herokuapp.com", name, secret)
+            val s3Client = S3Client(name)
+
+            logger.info("Creating evolver for name {}", name)
+            val maybeInitialIndividual = dudeClient.getLatestDude()
+
+            val evolverSettings = EvolverSettings(name)
+            val masterImage = ImageIO.read(File(evolverSettings.masterImageFile).toURI().toURL())
+            val evolver = makeEvolver(masterImage, maybeInitialIndividual, evolverSettings)
+
+            if (!GraphicsEnvironment.isHeadless()) {
+                val gui = GUI(masterImage)
+                evolver.addListener { gui.updateUiWithNewIndividual(it) }
+                gui.isVisible = true
+            }
+
+            // evolver.addListener { it.saveToDisk(name) }
+            evolver.addListener { dudeClient.postDude(it) }
+            evolver.addListener { s3Client.saveToS3(it); }
+
+            val geneticApplication = GeneticApplication(name, evolver)
+
+            return geneticApplication
+        }
+    }
+}
--- src/main/kotlin/com/cdpjenkins/genetic/Main.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/Main.kt
@@ -1,47 +1,14 @@
 package com.cdpjenkins.genetic
 
-import EvolverSettings
-import com.cdpjenkins.genetic.dudestore.client.DudeStoreClient
-import com.cdpjenkins.genetic.model.makeEvolver
-import com.cdpjenkins.genetic.persistence.S3Client
-import com.cdpjenkins.genetic.ui.GUI
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.awt.GraphicsEnvironment
-import java.io.File
-import javax.imageio.ImageIO
 
 val logger: Logger = LoggerFactory.getLogger(object{}::class.java)
 
 fun main(args: Array<String>) {
     val secret = System.getenv("SECRET")
-
     val name = args[0]
 
-    // TODO
-    // - Pull out the three things here into their own methods
-    // - Extract class GeneticEvolverApplication
-
-    logger.info("Creating evolver for name {}", name)
-
-    val dudeClient = DudeStoreClient("https://genetic-dude.herokuapp.com", name, secret)
-    val s3Client = S3Client(name)
-
-    val initialIndividual = dudeClient.getLatestDude()
-
-    val evolverSettings = EvolverSettings(name)
-    val masterImage = ImageIO.read(File(evolverSettings.masterImageFile).toURI().toURL())
-    val evolver = makeEvolver(masterImage, initialIndividual, evolverSettings)
-
-    if (!GraphicsEnvironment.isHeadless()) {
-        val gui = GUI(masterImage)
-        evolver.addListener { gui.updateUiWithNewIndividual(it) }
-        gui.isVisible = true
-    }
-
-//    evolver.addListener { it.saveToDisk(name) }
-    evolver.addListener { dudeClient.postDude(it) }
-    evolver.addListener { s3Client.saveToS3(it); }
-
-    evolver.start()
+    val application = GeneticApplication.create(name, secret)
+    application.start()
 }
