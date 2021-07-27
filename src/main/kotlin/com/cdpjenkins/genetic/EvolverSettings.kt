5d0ace2b47056ac467a84dd4526598dd5c7e088f
cdpjenkins <cdpjenkins@gmail.com>
2021-07-27 20:38:04 +0100

    Bigger max genome size

--- src/main/kotlin/com/cdpjenkins/genetic/EvolverSettings.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/EvolverSettings.kt
@@ -1,6 +1,6 @@
 class EvolverSettings(val name: String) {
     val initialGenomeSize = 0
-    val maxGenomeSize = 600.0
+    val maxGenomeSize = 1000.0
 
     val masterImageFile = "cow_half_size.jpg"
 
@@ -8,6 +8,6 @@ class EvolverSettings(val name: String) {
     val maxAlpha = 64
     val colourMutateAmount = 20
     val pointMutateRange = 10
-    val newShapeProbabilityFactor = 0.005
+    val newShapeProbabilityFactor = 0.001
     val avgShapesToMutate = 10.0
 }
