5b440e163584fdbbf8fef9811b74adab2e307b9d
Chris Jenkins <cdpjenkins@gmail.com>
2025-06-29 22:10:50 +0100

    First time touching the code in years; minor change to see if I can still deploy to Heroku

--- src/main/kotlin/com/cdpjenkins/genetic/Main.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/Main.kt
@@ -8,7 +8,8 @@ val logger: Logger = LoggerFactory.getLogger(object{}::class.java)
 fun main(args: Array<String>) {
     val secret = System.getenv("SECRET")
     val name = args[0]
+    val masterImageFileName = args[1]
 
-    val application = GeneticApplication.create(name, secret, args[1])
+    val application = GeneticApplication.create(name, secret, masterImageFileName)
     application.start()
 }
