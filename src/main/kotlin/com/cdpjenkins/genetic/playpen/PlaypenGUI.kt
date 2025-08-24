ec66fd587a8b7da30a18f2d3767285eb98f577d1
Chris Jenkins <cdpjenkins@gmail.com>
2025-08-24 08:54:21 +0100

    spike: Try out clikt for command line parsing

--- src/main/kotlin/com/cdpjenkins/genetic/playpen/PlaypenGUI.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/playpen/PlaypenGUI.kt
@@ -1,36 +1,58 @@
 package com.cdpjenkins.genetic.playpen
 
+import EvolverSettings
+import com.cdpjenkins.genetic.model.shape.BoundsRectangle
+import com.cdpjenkins.genetic.model.spawnRandomStrokedCubicCurveShape
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
 import java.awt.*
-import java.awt.geom.CubicCurve2D
-import java.awt.image.BufferedImage
 import javax.swing.*
-import kotlin.reflect.cast
 
-fun main(args: Array<String>) {
-    JFrame("Playpen").also {
+fun main(args: Array<String>) = PlaypenCommand().main(args)
 
-        it.add(PlayPenPanel().also {
-        })
+class PlaypenCommand : CliktCommand() {
+    val name by argument()
+    val numShapes by option("-n", "--num-shapes", help="Number of shapes to draw").int().default(10)
+    val width by option("-w", "--width", help="Width of the playpen").int().default(800)
+    val height by option("-h", "--height", help="Height of the playpen").int().default(800)
 
-        it.pack()
-        it.isVisible = true
+    override fun run() {
+        JFrame(name).also {
+            it.add(PlayPenPanel(numShapes, BoundsRectangle(0, 0, width, height)))
+
+            it.pack()
+            it.isVisible = true
+
+            it.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        }
     }
 }
 
-class PlayPenPanel: JPanel() {
+class PlayPenPanel(val numShapes: Int, val bounds: BoundsRectangle) : JPanel() {
     init {
-        preferredSize = Dimension(200, 200)
+        preferredSize = bounds.toDimension()
     }
 
     override fun paintComponent(g: Graphics?) {
         super.paintComponent(g)
         if (g is Graphics2D) {
-            val curve = CubicCurve2D.Double(10.0, 10.0, 100.0, 10.0, 10.0, 100.0, 100.0, 100.0)
-            val basicStroke = BasicStroke(4f, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)
-            g.setStroke(basicStroke)
-            g.draw(curve)
+            repeat(numShapes) {
+                drawRandomCubicCurve(g)
+            }
         } else {
             throw Exception("Arghghghghgh!")
         }
     }
+
+    private fun drawRandomCubicCurve(g: Graphics2D) {
+        spawnRandomStrokedCubicCurveShape(bounds, evolverSettings).draw(g)
+    }
 }
+
+private fun BoundsRectangle.toDimension() = Dimension(width, height)
+
+val evolverSettings = EvolverSettings.default("name")
