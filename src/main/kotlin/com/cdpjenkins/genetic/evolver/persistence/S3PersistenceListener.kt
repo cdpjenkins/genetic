59808af332fed3511d471e18b126cd83be01e705
Chris Jenkins <cdpjenkins@gmail.com>
2026-02-22 21:47:48 +0000

    refactor: Pull S3 persistence code into new S3Persister class

--- src/main/kotlin/com/cdpjenkins/genetic/evolver/persistence/S3PersistenceListener.kt
+++ src/main/kotlin/com/cdpjenkins/genetic/evolver/persistence/S3PersistenceListener.kt
@@ -11,49 +11,60 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
 
-class S3PersistenceListener(val name: String, val awsRegion: Region, val s3Bucket: String): Persister(), EvolverListener {
-    fun saveToS3(individual: Individual) {
+class S3PersistenceListener(val persister: S3Persister): EvolverListener {
+    override fun notify(individual: Individual) {
         if (individual.generation % 10 == 0) {
-            val region: Region = awsRegion
-            val s3: S3Client = S3Client.builder()
-                .region(region)
-                .build()
+            persister.saveToS3(individual)
+        }
+    }
+}
 
-            val baos = ByteArrayOutputStream()
-            ImageIO.write(individual.bufferedImage, "png", baos)
-            s3.putObject(
-                PutObjectRequest.builder()
-                    .bucket(s3Bucket)
-                    .key(pngFileName(individual, name))
-                    .build(),
-                RequestBody.fromBytes(baos.toByteArray())
-            )
+class S3Persister(private val s3Client: S3Client, val name: String, val s3Bucket: String) : Persister() {
+    companion object {
+        fun create(name: String): S3Persister {
+            val s3Client: S3Client = S3Client.builder().region(Region.EU_WEST_1).build()
 
-            s3.putObject(
-                PutObjectRequest.builder()
-                    .bucket(s3Bucket)
-                    .key(jsonFileName(individual, name))
-                    .build(),
-                RequestBody.fromString(serialise(individual))
-            )
-
-            val svgString = SvgRenderer().renderToString(individual)
-            val svgFile = svgFileName(individual, name)
-            s3.putObject(
-                PutObjectRequest.builder()
-                    .bucket(s3Bucket)
-                    .key(svgFile)
-                    .build(),
-                RequestBody.fromString(svgString)
-            )
+            return S3Persister(s3Client, name, "cdpjenkins-bovine-assets")
         }
     }
 
-    override fun notify(individual: Individual) {
-        saveToS3(individual)
+    fun saveToS3(individual: Individual) {
+        s3Client.writeJSON(individual)
+        s3Client.writePNG(individual)
+        s3Client.writeSVG(individual)
     }
 
-    companion object {
-        fun create(name: String): S3PersistenceListener = S3PersistenceListener(name, Region.EU_WEST_1, "cdpjenkins-bovine-assets")
+    private fun S3Client.writeSVG(individual: Individual) {
+        val svgString = SvgRenderer().renderToString(individual)
+        val svgFile = svgFileName(individual, name)
+        putObject(
+            PutObjectRequest.builder()
+                .bucket(s3Bucket)
+                .key(svgFile)
+                .build(),
+            RequestBody.fromString(svgString)
+        )
+    }
+
+    private fun S3Client.writeJSON(individual: Individual) {
+        putObject(
+            PutObjectRequest.builder()
+                .bucket(s3Bucket)
+                .key(jsonFileName(individual, name))
+                .build(),
+            RequestBody.fromString(serialise(individual))
+        )
+    }
+
+    private fun S3Client.writePNG(individual: Individual) {
+        val baos = ByteArrayOutputStream()
+        ImageIO.write(individual.bufferedImage, "png", baos)
+        putObject(
+            PutObjectRequest.builder()
+                .bucket(s3Bucket)
+                .key(pngFileName(individual, name))
+                .build(),
+            RequestBody.fromBytes(baos.toByteArray())
+        )
     }
 }
